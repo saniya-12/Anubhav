9467719a24c08273408bb5ab3e4c6e96712aace4
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-03-06 10:49:35 +0530

    Angular Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { C3Component } from './c3/c3.component';
+import { ParentComponent } from './parent/parent.component';
 
 @Component({ //Decorator
   selector: 'demo',
   standalone: true,
-  imports: [RouterOutlet,C3Component],
+  imports: [RouterOutlet,C3Component, ParentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
