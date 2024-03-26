8ec99fe77be61d940c7d170551da5ccdf8d2fc7b
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-03-26 23:32:11 +0530

    ngIf

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,17 +1,20 @@
-import { NgClass, NgFor } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { movie } from './movie';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [NgFor,NgClass],
+  imports: [NgFor,NgClass, NgIf, FormsModule],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.css'
 })
 export class MoviesComponent {
   movies:movie[]=[{name:"3 Idiots", year:2000},{name:"Shaitaan", year:2024},{name:"YJHD", year:2006}]
   // ctrl + space
+
+  showMe:boolean = false;
 
 }
