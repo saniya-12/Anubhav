47691a04b1a3cd68be922a3af99e71c41c42daf7
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-03 10:25:31 +0530

    child updated

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,7 +7,30 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './child.component.html',
   styleUrl: './child.component.css'
 })
-export class ChildComponent {
+// export class ChildComponent {
+//   @Input() amount: number = 0
+//   @Output() amountChange : EventEmitter<number> = new EventEmitter<number>;
+
+//   @Input() spent: number = 0;
+//   @Output() spentChange : EventEmitter<number> = new EventEmitter<number>;
+
+//   withdraw(){
+//     this.amount -=100;
+//     this.spent +=100;
+//     this.amountChange.emit(this.amount);
+//     this.spentChange.emit(this.spent);
+//   }
+// }
+export class ChildComponent implements DoCheck, OnChanges, OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
+  ngDoCheck(): void {
+    throw new Error('Method not implemented.');
+  }
   @Input() amount: number = 0
   @Output() amountChange : EventEmitter<number> = new EventEmitter<number>;
 
