6985b2b00c4995aecd047cdbaa59f58683f25b27
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-08 08:43:14 +0530

    routing practice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MoviesComponent } from './movies/movies.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { C3Component } from './c3/c3.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'movies', component:MoviesComponent},
+    { path:'pipes', component:PipesComponent},
+    { path:'c3', component:C3Component}
+];
+
+// ng g c componentname
