3b1bf43dde139d3df5afa02b350e110a60f72aa4
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-08 08:59:34 +0530

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,11 @@ import { C3Component } from './c3/c3.component';
 
 export const routes: Routes = [
     { path: 'movies', component:MoviesComponent},
-    { path:'pipes', component:PipesComponent},
-    { path:'c3', component:C3Component}
+    // { path:'pipes', component:PipesComponent},
+    { path:'c3', component:C3Component},
+    { path:'', component:PipesComponent}, // it will start by default. 
+    // {path:'login', redirectTo: 'PipesComponent'}
+    {path:'login', redirectTo: 'c3'}
 ];
 
 // ng g c componentname
