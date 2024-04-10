f133037f19b19e6e3dd818d0f7c63471fa89cb18
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-10 10:30:50 +0530

    update

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-observables',
@@ -10,20 +10,36 @@ import { Observable } from 'rxjs';
 })
 export class ObservablesComponent implements OnInit{
   ngOnInit(): void {
-    this.myObservable.subscribe({
-      next:(val) => console.log(val),
-      error:(err) => console.log(err),
-      complete:() => console.log('completed')
-    })
-  }
-  myObservable = new Observable((observer) => {
-    console.log('Observable Starts');
+  //   this.myObservable.subscribe({
+  //     next:(val) => console.log(val),
+  //     error:(err) => console.log(err),
+  //     complete:() => console.log('completed')
+  //   })
+  // }
+  // myObservable = new Observable((observer) => {
+  //   console.log('Observable Starts');
 
-    setTimeout(() => observer.next(1),1000)
-    setTimeout(() => observer.next(2),1000)
-    setTimeout(() => observer.next(3),1000)
-    setTimeout(() => observer.next(4),1000)
-    observer.next(17)
+  //   setTimeout(() => observer.next(1),1000)
+  //   setTimeout(() => observer.next(2),1000)
+  //   setTimeout(() => observer.next(3),1000)
+  //   setTimeout(() => observer.next(4),1000)
+  //   observer.next(17)
     //observer.error('error generated');
-  })
+    
+    const array1 = [1,2,3,4,5]
+    const array2 = ['a', 'b', 'c', 'd', 'e']
+    const obsof2 = of(array1, array2)
+
+    obsof2.subscribe({
+      next:(num:any) => {
+        console.log(num)
+      },
+      error: (err:any) => {
+        console.log("Error in streaming" + err)
+      },
+      complete: () => {
+        console.log('Completed')
+      }
+    })
+  } // )
 }
